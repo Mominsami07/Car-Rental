62194df1fcee224ba64c9b05cf09ee802615d370
mominsami07 <momensami009@gmail.com>
2022-08-13 13:15:07 +0200

    fix sweet alert

--- CarRent/Startup.cs
+++ CarRent/Startup.cs
@@ -50,7 +50,7 @@ namespace CarRent
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Rentals}/{action=Index}/{id?}");
             });
         }
     }
